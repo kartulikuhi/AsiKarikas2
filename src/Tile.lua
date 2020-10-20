c1d01349bd1cfb525403375cba3a9bf3048bc7bf
unknown <marten@blade.int.degeetia.ee>
2020-10-20 20:00:46 +0300

    Correct Files

---
+++ src/Tile.lua
@@ -0,0 +1,46 @@
+--[[
+    GD50
+    Match-3 Remake
+
+    -- Tile Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The individual tiles that make up our game board. Each Tile can have a
+    color and a variety, with the varietes adding extra points to the matches.
+]]
+
+Tile = Class{}
+
+function Tile:init(x, y, color, variety, shiny)
+    
+    -- board positions
+    self.gridX = x
+    self.gridY = y
+    self.isShiny = shiny
+    -- coordinate positions
+    self.x = (self.gridX - 1) * 32
+    self.y = (self.gridY - 1) * 32
+
+    -- tile appearance/points
+    self.color = color
+    self.variety = variety
+end
+
+function Tile:render(x, y)
+    
+    -- draw shadow
+    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety], self.x + x + 2, self.y + y + 2)
+
+    -- draw tile itself
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x, self.y + y)
+    if self.isShiny then
+        love.graphics.setColor(255, 223, 0, 100)
+        love.graphics.circle('fill', self.x + x + 16, self.y + y + 16, 4)
+        love.graphics.setColor(255, 255, 255, 255)
+    end
+end
